d1ecc80da76db818203ce46eb88fa54983150a0c
aporcam0410 <aporcam0410@g.educaand.es>
2026-05-18 13:00:02 +0200

    Fix 404 en rutas SPA y anadir lang passwords es

---
+++ Laravel/lang/es/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset'     => 'Tu contraseña ha sido restablecida.',
+    'sent'      => 'Te hemos enviado el enlace para restablecer tu contraseña.',
+    'throttled' => 'Por favor espera antes de intentarlo de nuevo.',
+    'token'     => 'El enlace de restablecimiento no es válido o ha expirado.',
+    'user'      => 'No encontramos ninguna cuenta con ese email.',
+];
